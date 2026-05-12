96907a64b142e1549003d9972bce4fda90ac9543
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-12 13:33:41 +0600

    feat(sql): solve LC-577. Employee Bonus

---
+++ Basic Joins/E577. Employee Bonus.sql
@@ -0,0 +1,14 @@
+-- Problem: Employee Bonus
+-- R.Beats: 43.92%
+
+SELECT
+    e.name,
+    b.bonus
+FROM
+    Employee AS e
+LEFT JOIN
+    Bonus AS b
+    ON e.empId = b.empId
+WHERE
+    b.bonus IS NULL
+    OR b.bonus < 1000;
