44e020d352bd164aec4dad941a487d68537e79e8
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-12 23:33:27 +0600

    feat(sql): solve LC-1211. Queries Quality and Percentage

---
+++ Basic Aggregate Functions/E1211. Queries Quality and Percentage.sql
@@ -0,0 +1,22 @@
+-- Problem: Queries Quality and Percentage
+-- R.Beats: 63.82%
+
+SELECT
+    query_name,
+    ROUND(
+        AVG(rating / position),
+        2
+    ) AS quality,
+    ROUND(
+        AVG(
+            CASE
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END
+        ) * 100,
+        2
+    ) AS poor_query_percentage
+FROM
+    Queries
+GROUP BY
+    query_name;
