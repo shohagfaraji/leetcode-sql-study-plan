88c65bf3ebcb3ac4daa3f548c24051ead95a2869
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-13 16:58:39 +0600

    feat(sql): solve LC-1729. Find Followers Count

---
+++ Sorting and Grouping/E1729. Find Followers Count.sql
@@ -0,0 +1,12 @@
+-- Problem: Find Followers Count
+-- R.Beats: 70.18%
+
+SELECT
+    user_id,
+    COUNT(follower_id) AS followers_count
+FROM
+    Followers
+GROUP BY
+    user_id
+ORDER BY
+    user_id ASC;
