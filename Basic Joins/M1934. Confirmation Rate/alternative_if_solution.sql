d6bfd04e5e69d36bc4811a864c5b33f0340c64ae
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-15 00:38:12 +0600

    feat(sql): solve LC-1934. Confirmation Rate using IF

---
+++ Basic Joins/M1934. Confirmation Rate/alternative_if_solution.sql
@@ -0,0 +1,18 @@
+-- Problem: Confirmation Rate
+-- R.Beats: 36.19%
+
+SELECT
+    s.user_id,
+    ROUND(
+        AVG(
+            IF(c.action = 'confirmed', 1, 0)
+        ),
+        2
+    ) AS confirmation_rate
+FROM
+    Signups AS s
+LEFT JOIN
+    Confirmations AS c
+    ON s.user_id = c.user_id
+GROUP BY
+    s.user_id;
