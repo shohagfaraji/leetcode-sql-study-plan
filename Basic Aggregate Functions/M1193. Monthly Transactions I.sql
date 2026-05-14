41270991d41c43bf62775d538c1bf4b4b38f6d53
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-15 01:15:46 +0600

    feat(sql): solve LC-1193. Monthly Transactions I

---
+++ Basic Aggregate Functions/M1193. Monthly Transactions I.sql
@@ -0,0 +1,15 @@
+-- Problem: Monthly Transactions I
+-- R.Beats: 59.24%
+
+SELECT
+    DATE_FORMAT(trans_date, '%Y-%m') AS month,
+    country,
+    COUNT(*) AS trans_count,
+    SUM(IF(state = 'approved', 1, 0)) AS approved_count,
+    SUM(amount) AS trans_total_amount,
+    SUM(IF(state = 'approved', amount, 0)) AS approved_total_amount
+FROM
+    Transactions
+GROUP BY
+    DATE_FORMAT(trans_date, '%Y-%m'),
+    country;
