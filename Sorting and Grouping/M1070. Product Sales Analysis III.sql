f287a1697e3360c7a3407ca65393024370702d9e
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-15 19:04:32 +0600

    feat(sql): solve LC-1070. Product Sales Analysis III

---
+++ Sorting and Grouping/M1070. Product Sales Analysis III.sql
@@ -0,0 +1,22 @@
+-- Problem: Product Sales Analysis III
+-- R.Beats: 69.36%
+
+SELECT
+    s.product_id,
+    s.year AS first_year,
+    s.quantity,
+    s.price
+FROM
+    Sales AS s
+JOIN (
+    SELECT
+        product_id,
+        MIN(year) AS first_year
+    FROM
+        Sales
+    GROUP BY
+        product_id
+) AS g
+ON
+    s.product_id = g.product_id
+    AND s.year = g.first_year;
