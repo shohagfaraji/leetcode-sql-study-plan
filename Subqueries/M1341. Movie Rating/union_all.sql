bf62901714545b9e22eccae4fb992e9c9c156645
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-16 23:18:23 +0600

    feat(sql): solve LC-1341 using UNION ALL optimized approach

---
+++ Subqueries/M1341. Movie Rating/union_all.sql
@@ -0,0 +1,39 @@
+-- Problem: Movie Rating
+-- R.Beats: 79.16%
+
+(
+    SELECT
+        u.name AS results
+    FROM
+        Users AS u
+    JOIN MovieRating AS mr
+        ON u.user_id = mr.user_id
+    GROUP BY
+        u.user_id,
+        u.name
+    ORDER BY
+        COUNT(*) DESC,
+        u.name ASC
+    LIMIT 1
+)
+
+UNION ALL
+
+(
+    SELECT
+        m.title AS results
+    FROM
+        Movies AS m
+    JOIN MovieRating AS mr
+        ON m.movie_id = mr.movie_id
+    WHERE 
+        mr.created_at >= '2020-02-01'
+        AND mr.created_at < '2020-03-01'
+    GROUP BY
+        m.movie_id,
+        m.title
+    ORDER BY
+        AVG(mr.rating) DESC,
+        m.title ASC
+    LIMIT 1
+);
