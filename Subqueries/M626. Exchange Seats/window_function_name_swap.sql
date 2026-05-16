0220259deb65f93367eb4694ee5bdf42424daadf
Shohag Faraji <shohagfaraji2@gmail.com>
2026-05-16 23:16:49 +0600

    feat(sql): solve LC-626 using LEAD/LAG name swap approach

---
+++ Subqueries/M626. Exchange Seats/window_function_name_swap.sql
@@ -0,0 +1,23 @@
+-- Problem: Exchange Seats
+-- R.Beats: 52.12%
+
+SELECT
+    id,
+    CASE
+        WHEN id % 2 = 1
+            AND id != (
+                SELECT
+                    MAX(id)
+                FROM
+                    Seat
+            )
+        THEN LEAD(student) OVER (ORDER BY id)
+
+        WHEN id % 2 = 0
+        THEN LAG(student) OVER (ORDER BY id)
+        ELSE student
+    END AS student
+FROM
+    Seat
+ORDER BY
+    id;
